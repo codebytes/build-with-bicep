23d886edd554df01688611d3628597596d478005
Chris Ayers <clayers@gmail.com>
2022-06-24 09:27:12 -0400

    updated samples

---
+++ 12-container-registries/create-Acr.sh
@@ -0,0 +1,13 @@
+acrname='cayersbicepacr'
+rg='rg-bicep'
+az acr create -g $rg --name $acrname --sku Basic --location eastus
+
+az acr repository list --name $acrname --output table
+
+az bicep publish \
+  --file website.bicep \
+  --target 'br:$acrname.azurecr.io/website:v1'
+
+az bicep publish \
+  --file cdn.bicep \
+  --target 'br:$acrname.azurecr.io/cdn:v1'
